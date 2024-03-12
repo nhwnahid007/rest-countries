6b721627ccdf48fbefe85a8756fe521502285d9d
nhwnahid007 <nhwnahid@gmail.com>
2024-03-12 06:27:01 +0600

    Added flag images

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -5,18 +5,56 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries,setCountries] =useState([]);
+    const [visitedCountries,setVisitedCountries] = useState([]);
+    const [visitedFlags,setVistedFlags] =useState([]);
     useEffect(()=>{
         fetch("https://restcountries.com/v3.1/all")
         .then (res => res.json())
         .then (data => setCountries(data))
     },[])
+    const handleVisitedCountry = country =>{
+      console.log('country');
+      const newVisitedCountrie=[...visitedCountries,country];
+      setVisitedCountries(newVisitedCountrie)
+    }
+    const handleVisitedFlags =flag =>{
+      const newVisitedFlags = [...visitedFlags,flag];
+      setVistedFlags(newVisitedFlags)
+    }
+    //remove item from an arrray in a state  
+    //use filter to select all the elements 
+
+
+
     return (
       <div>
         <h3>Total Countries:{countries.length} </h3>
+        <div>
+          <h5>Visited Countries: {visitedCountries.length} </h5>
+          {/* Visited country */}
+        </div>
+        <div>
+          <ul>
+            {
+              visitedCountries.map(country=> <li key={country.cca3}>{country.name.common}</li>)
+            }
+          </ul>
+        </div>
+        <div className="flag-container">
+            {
+              visitedFlags.map((flag,idx)=> <img key={idx} src={flag}></img>)
+            }
+        </div>
+        {/* display countries */}
         <div className="country-container">
           {
             countries.map((country) => (
-              <Country key={country.cca3} country={country}></Country>
+              <Country
+                key={country.cca3}
+                handleVisitedCountry={handleVisitedCountry}
+                handleVisitedFlags ={handleVisitedFlags}
+                country={country}
+              ></Country>
             ))
             // key na dile eslint error dekhaabe ejnno unique jinis key hisebe set kore dite hobe
             // props pathate hobe tai country k pathiye dilam
--- src/components/counttry/Country.jsx
+++ src/components/counttry/Country.jsx
@@ -1,25 +1,48 @@
-import { useState } from 'react';
-import './Country.css'
+import { useState } from "react";
+import "./Country.css";
 // props k destructuring korbo ta {country diye dilam}
-const Country = ({country}) => {
-    const {name,maps,flags,population,area,cca3} = country; //country er name property destructure
-    const [visited,setVisited] = useState(false)
-    const handleVisited=()=>{
-     setVisited(!visited);
-    }
-    return (
-      <div className="country">
-        <h3>Name: {name?.common} </h3>
-        <img src={flags.png} alt="" /> <br />
-        <p>population: {population} {}</p>
-        <p>Area: {area}</p><p><small>Code: {cca3}</small></p>
-        <a href={maps.googleMaps}><button>Google map</button> </a> <br />
-        <button onClick={handleVisited}>{visited? 'Visited': 'Not visited'}</button>
-        {visited ? ' This country' : 'Not visited'}
-        {/* visited jodi true hoy */}
-      </div>
-      
-    );
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags}) => {
+  const { name, maps, flags, population, area, cca3 } = country; //country er name property destructure
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+
+  return (
+    <div  className={`country ${visited && "visited"}`}>
+      <h3 style={{ color: visited ? "purple" : "gray" }}>
+        Name: {name?.common}{" "}
+      </h3>
+      <img src={flags.png} alt="" /> <br />
+      <p>
+        population: {population} {}
+      </p>
+      <p>Area: {area}</p>
+      <p>
+        <small>Code: {cca3}</small>
+      </p>
+      <a target="_blank" href={maps.googleMaps}>
+        <button style={{ backgroundColor: visited ? "orange" : "yellow" }}>
+          Google map
+        </button>{" "}
+      </a>{" "}
+      <br />
+      <button className="gap"
+        onClick={() => {
+          handleVisitedCountry(country);
+        }}
+      >
+        Mark as visited
+      </button> <br />
+      <button onClick={()=>handleVisitedFlags(country.flags.png)} className="gap">Add flag</button>
+      <br />
+      <button className="gap" onClick={handleVisited}>
+        {visited ? "Visited" : "Not visited"}
+      </button>
+      {visited ? " This country" : "Not visited"}
+      {/* visited jodi true hoy */}
+    </div>
+  );
 };
 
 export default Country;
